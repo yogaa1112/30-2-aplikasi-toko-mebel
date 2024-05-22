d4b9debcbc2e85a1509ef9a91e01e900ab2c6fc5
Fery Ale <feryalesmana@gmail.com>
2024-05-22 11:29:34 +0700

    Mengkonfigurasi database dan menyimpan gambar menggunakan multer

--- Back-End/index.js
+++ Back-End/index.js
@@ -1,6 +1,48 @@
+const port = 4000;
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const path = require('path');
 const cors = require('cors');
 const multer = require('multer');
 const jwt = require('jsonwebtoken');
 
+app.use(express.json());
+app.use(cors());
+
+//Koneksi ke database mongoDB
+mongoose.connect('mongodb+srv://fery-ale:Capstone@cluster0.c8jg4an.mongodb.net/mebel')
+
+app.get('/', (req,res)=>{
+    res.send('App berjalan')
+})
+
+//Penyimpanan images
+const storage = multer.diskStorage({
+    destination: './upload/images',
+    filename: (req, file, cb)=>{
+        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    }
+})
+
+const upload = multer({storage:storage})
+
+// membuat upload enpoint
+app.use('/images', express.static('upload/images'))
+
+app.post('/upload', upload.single('produk'),(req,res)=>{
+    res.json({
+        success : 1,
+        image_url : `http://localhost:${port}/images/${req.file.filename}`
+    })
+})
+
+app.listen(port, (err)=>{
+    if(!err){
+        console.log("Server berjalan pada port " +port)
+    }
+    else
+    {
+        console.log("Server tidak berjalan"+err)
+    }
+})
