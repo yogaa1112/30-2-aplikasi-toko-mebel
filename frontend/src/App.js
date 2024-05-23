d63f6d73fb371a67291df5dbb922801b3bb1c17b
AdePrasTyo <adeprasestyo6@gmail.com>
2024-05-23 17:27:12 +0700

    Menambahkan Pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,24 @@
-import './App.css';
-import Navbar from './Components/Navbar/Navbar';
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Navbar />
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Shop />} />
+          <Route path="/mens" element={<ShopCategory category="men" />} />
+          <Route path="/womens" element={<ShopCategory category="women" />} />
+          <Route path="/kids" element={<ShopCategory category="kid" />} />
+          <Route path="/product" element={<Product />}>
+            <Route path="/productId" element={<Product />} />
+          </Route>
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/Login" element={<LoginSignup />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ frontend/src/Pages/Cart.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Cart = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Cart
