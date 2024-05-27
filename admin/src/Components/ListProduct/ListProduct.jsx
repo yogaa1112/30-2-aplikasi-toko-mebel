7a0f10889d2727ad308a5b372d22f6a5af0db49a
tiaradewi31 <tiaradewiibnatus2100686@upi.edu>
2024-05-27 21:13:16 +0700

    menambahkan list product

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, { useEffect,useState } from 'react'
 import './ListProduct.css'
+import remove_icon from '../../assets/remove-icon.png'
 
 const ListProduct = () => {
+
+    const [allproducts,setAllProducts] = useState([]);
+
+    const fetchInfo = async ()=>{
+      await fetch ('http://localhost:3000/allproducts')
+      .then((res)=>res.json())
+      .then((data)=>{setAllProducts(data)});
+    }
+
+    useEffect(()=>{
+      fetchInfo();
+    }, [])
+
+    const remove_product = async()=>{
+      await fetch('http://localhost:3000/removeproduct', {
+        method:'POST',
+        headers:{
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body:JSON.stringify({id:id})
+      })
+      await fetchInfo();
+    }
+
   return (
     <div className='list-product'>
        <h1>All Mebelify Product List</h1>
@@ -15,6 +41,19 @@ const ListProduct = () => {
        </div>
        <div className="listproduct-allproduct">
         <hr />
+          {allproducts.map((product,index)=>{
+            return <> 
+            <div key={index} className="listproduct-format-main listproduct-format">
+              <img src={product.image} alt="" className="listproduct-product-icon" />
+              <p>{product.name}</p>
+              <p>${product.old_price}</p>
+              <p>${product.new_price}</p>
+              <p>{product.category}</p>
+              <img onClick={()=>{remove_product(product.id)}} src={remove_icon} alt="" className="listproduct-remove-icon" />
+            </div>
+            <hr />
+            </>
+          })}
        </div>
     </div>
   )
