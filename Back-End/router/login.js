1a12e541c0ecf6c5f9b1067982bf23eb1eb46a0f
FeryAlesmana <146193433+FeryAlesmana@users.noreply.github.com>
2024-06-06 12:14:21 +0700

    Delete Back-End directory

--- Back-End/router/login.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const login = require('../controller/login-controller.js')
-
-//Membuat API untuk User login
-router.post('/', login);
-
-module.exports = router;
