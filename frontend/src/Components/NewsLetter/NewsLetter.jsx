fda106e8c0e70fc86da52c65ed334f2eb9177e8c
AdePrasTyo <adeprasestyo6@gmail.com>
2024-06-09 01:12:30 +0700

    Perbaikan tampilan
    Tampilan tata letak item dan lainnya

--- frontend/src/Components/NewsLetter/NewsLetter.jsx
+++ frontend/src/Components/NewsLetter/NewsLetter.jsx
@@ -4,12 +4,12 @@ import './NewsLetter.css'
 const NewsLetter = () => {
   return (
     <div className='newsletter'>
-        <h1>Get Exclusive Ofeers On Your Email</h1>
-        <p>Subscribe to our newletter and stay updated</p>
-        <div>
-            <input type="email" placeholder='your email id' />
-            <button>Subscribe</button>
-        </div>
+      <h1>Get Exclusive Offers On Your Email</h1>
+      <p>Subscribe to our newletter and stay updated</p>
+      <div>
+        <input type="email" placeholder='Your email address' />
+        <button>Send</button>
+      </div>
     </div>
   )
 }
