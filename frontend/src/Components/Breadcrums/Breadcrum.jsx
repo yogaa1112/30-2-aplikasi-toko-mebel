8f2f201257626dbe803b3b10f5e0812069870e37
Muhammad Wisnu Ainun Najib <135032586+MwanNajib@users.noreply.github.com>
2024-06-09 21:44:45 +0700

    nambahin css

--- frontend/src/Components/Breadcrums/Breadcrum.jsx
+++ frontend/src/Components/Breadcrums/Breadcrum.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Breadcrum.css'
-import arrow_icon from '../Assets/dropdown_icon.png'
+import arrow_icon from '../Assets/breadcrum_arrow.png'
 
 export const Breadcrum = (props) => {
     const {product} = props;
