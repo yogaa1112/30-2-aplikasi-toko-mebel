86dbf9185f052318f29c0fa80adedb3befd16b82
Muhammad Wisnu Ainun Najib <135032586+MwanNajib@users.noreply.github.com>
2024-06-10 21:27:13 +0700

    login fitur

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -4,10 +4,10 @@ import logo from '../Assets/logo-text.png'
 import cart_icon from '../Assets/cart_icon.png'
 import search_icon from '../Assets/search_icon.png'
 import people_icon from '../Assets/people_icon.png'
+import logout_icon from '../Assets/logout.png'
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
-
     const [menu, setMenu] = useState("shop");
     return (
         <div className="navbar">
@@ -15,21 +15,38 @@ const Navbar = () => {
                 <Link to='/'><img src={logo} alt="logo" /></Link>
             </div>
             <ul className="nav-menu">
-                <li onClick={() => { setMenu("shop") }}><Link style={{ textDecoration: 'none', color: 'black' }} to='/'>Shop</Link>{menu === "shop" ? <hr /> : <></>}</li>
-                <li onClick={() => { setMenu("home") }}><Link style={{ textDecoration: 'none', color: 'black' }} to='/home'>Home</Link>{menu === "home" ? <hr /> : <></>}</li>
-                <li onClick={() => { setMenu("office") }}><Link style={{ textDecoration: 'none', color: 'black' }} to='/office'>Office</Link>{menu === "office" ? <hr /> : <></>}</li>
-                <li onClick={() => { setMenu("kitchen") }}><Link style={{ textDecoration: 'none', color: 'black' }} to='/kitchen'>Kitchen</Link>{menu === "kitchen" ? <hr /> : <></>}</li>
+                <li onClick={() => { setMenu("shop") }}>
+                    <Link style={{ textDecoration: 'none', color: 'black' }} to='/'>Shop</Link>
+                    {menu === "shop" ? <hr /> : null}
+                </li>
+                <li onClick={() => { setMenu("home") }}>
+                    <Link style={{ textDecoration: 'none', color: 'black' }} to='/home'>Home</Link>
+                    {menu === "home" ? <hr /> : null}
+                </li>
+                <li onClick={() => { setMenu("office") }}>
+                    <Link style={{ textDecoration: 'none', color: 'black' }} to='/office'>Office</Link>
+                    {menu === "office" ? <hr /> : null}
+                </li>
+                <li onClick={() => { setMenu("kitchen") }}>
+                    <Link style={{ textDecoration: 'none', color: 'black' }} to='/kitchen'>Kitchen</Link>
+                    {menu === "kitchen" ? <hr /> : null}
+                </li>
             </ul>
             <div className="nav-search-login-cart">
                 <div className="nav-search">
                     <input type="text" placeholder="Cari..." />
                     <Link to=''><img src={search_icon} alt="search" id="search_icon" /></Link>
                 </div>
-                <Link to='/login'><img src={people_icon} alt="login" id="login-icon" /></Link>
+                {localStorage.getItem('auth-token')
+                    ? <button onClick={() => { localStorage.removeItem('auth-token'); window.location.replace("/") }}>
+                        <img src={logout_icon} alt="logout" id="logout-icon" />
+                      </button>
+                    : <Link to='/login'><img src={people_icon} alt="login" id="login-icon" /></Link>
+                }  
                 <Link to='/cart'><img src={cart_icon} alt="cart" id="cart-icon" /></Link>
             </div>
         </div>
-    )
+    );
 }
 
-export default Navbar
+export default Navbar;
