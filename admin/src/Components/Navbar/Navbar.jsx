a62dbe6c570f47cdc0458d965f5c1734f9436dec
Fery Ale <feryalesmana@gmail.com>
2024-06-11 14:12:07 +0700

    fixing admin page

--- admin/src/Components/Navbar/Navbar.jsx
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -4,9 +4,14 @@ import navlogo from '../../assets/logo-admin-mebelify.png'
 import navProfile from '../../assets/navbar-profile.png'
 
 const Navbar = () => {
+  const handleLogoClick = () => {
+    localStorage.removeItem('auth-token')
+    window.location.href = 'http://localhost:3000';
+    
+  };
   return (
     <div className='navbar'>
-        <img src={navlogo} onClick={window.location.href = 'http://localhost:3000'} alt="" className="nav-logo" />
+        <img src={navlogo} onClick={handleLogoClick} alt="" className="nav-logo" />
         <img src={navProfile} className='nav-profile' alt="" />
     </div>
   )
