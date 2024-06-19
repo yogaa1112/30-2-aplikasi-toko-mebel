abbdd9ecfc709e7f1b06453d702ddcb9698b33cf
Fery Ale <feryalesmana@gmail.com>
2024-06-19 11:57:08 +0700

    Memperbaiki format Harga

--- frontend/src/Components/CartItems/CartItems.jsx
+++ frontend/src/Components/CartItems/CartItems.jsx
@@ -6,7 +6,13 @@ import remove_icon from '../Assets/cross_cart.png'
 import CheckOutButton from '../CheckOutButton/CheckOutButton.jsx'
 const CartItems = () => {
     const {getTotalCartAmount, all_product, cartItems,removeFromCart} = useContext(ShopContext)
-    
+    const formatPrice = (price) => {
+      return new Intl.NumberFormat('id-ID', {
+        style: 'currency',
+        currency: 'IDR',
+        minimumFractionDigits: 0
+      }).format(price).replace('IDR', 'Rp');
+    }
     const cartItemsList = all_product.filter(cartItem => cartItems[cartItem.id] > 0)
     .map(cartItem => {
       const { _id, __v, date, ...item } = cartItem; 
@@ -30,9 +36,9 @@ const CartItems = () => {
             <div className="cartitems-format cartitems-format-main">
                 <img src={cartItem.image} alt="" className='carticon-product-icon' />
                 <p>{cartItem.name}</p>
-                <p>Rp.{cartItem.price}</p>
+                <p>{formatPrice(cartItem.price)}</p>
                 <span className='cartitems-quantity'>{cartItems[cartItem.id]}</span>
-                <p>Rp.{cartItem.price*cartItems[cartItem.id]}</p>
+                <p>{formatPrice(cartItem.price*cartItems[cartItem.id])}</p>
                 <img className='cartitems-remove-icon' src={remove_icon} onClick={()=>{removeFromCart(cartItem.id)}} alt="" />
             </div>
             <hr />
@@ -44,7 +50,7 @@ const CartItems = () => {
         <div className="cartitems-total">
             <div className="cartitems-total-item">
                 <p>Subtotal</p>
-                <p>Rp.{getTotalCartAmount()}</p>
+                <p>{formatPrice(getTotalCartAmount())}</p>
             </div>
             <hr />
             <div className="cartitems-total-item">
@@ -54,7 +60,7 @@ const CartItems = () => {
             <hr />
             <div className="cartitems-total-item">
                 <p>Total</p>
-                <h3>Rp.{getTotalCartAmount()}</h3>
+                <h3>{formatPrice(getTotalCartAmount())}</h3>
             </div>
             {cartItemsList.length > 0 ? (
             <CheckOutButton cartItems={cartItemsList} />
